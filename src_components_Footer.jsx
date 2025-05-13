e80f339909ecfdffa1aa4d726efc543cab024bce
Estpital <info@estpital.com>
2025-05-13 22:23:01 +0300

    Delete src_components_Footer.jsx

--- src_components_Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-blue-900 text-white p-4 mt-8">
-      <div className="container mx-auto text-center">
-        <p>© 2025 Estpital. Tüm hakları saklıdır.</p>
-      </div>
-    </footer>
-  );
-}
